5734033299590597cbedbebc4e8e5d3b5fac341c
gsavin <guilhelm.savin@graphstream-project.org>
2012-04-03 14:06:39 +0200

    Update start script

--- start.sh
+++ start.sh
@@ -7,15 +7,16 @@ else
 fi
 
 M2="/home/raziel/.m2/repository"
-ECLIPSE_PROJECT="gs-core gs-boids d3"
-LIBS="../d3/lib/javassist.jar $M2/org/graphstream/pherd/1.0/pherd-1.0.jar $M2/org/graphstream/mbox2/1.0/mbox2-1.0.jar"
+LIBS="$M2/org/graphstream/gs-core/1.2-git/gs-core-1.2-git.jar \
+$M2/org/graphstream/gs-algo/1.2-git/gs-algo-1.2-git.jar \
+$M2/org/graphstream/gs-boids/git/gs-boids-git.jar \
+$M2/org/d3/d3-core/1.0-git/d3-core-1.0-git.jar \
+$M2/org/javassist/javassist/3.14.0-GA/javassist-3.14.0-GA.jar \
+$M2/org/graphstream/pherd/1.0/pherd-1.0.jar \
+$M2/org/graphstream/mbox2/1.0/mbox2-1.0.jar"
 MAIN="org.d3.app.boids.StartBoid"
 JVM_OPTS="-Dorg.d3.config=$CFG -Dsun.java2d.opengl=true"
 CLASSPATH="bin/"
-
-for PROJECT in $ECLIPSE_PROJECT; do
-	CLASSPATH+=":../$PROJECT/bin/"
-done
 
 for LIB in $LIBS; do
 	CLASSPATH+=":$LIB"
