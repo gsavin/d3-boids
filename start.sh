3f88e83a7e892cb12ad8e0b38fac3ef70c680f9a
gsavin <guilhelm.savin@graphstream-project.org>
2012-04-05 18:05:12 +0200

    Update config

--- start.sh
+++ start.sh
@@ -10,13 +10,12 @@ M2="$HOME/.m2/repository"
 LIBS="$M2/org/graphstream/gs-core/1.2-git/gs-core-1.2-git.jar \
 $M2/org/graphstream/gs-algo/1.2-git/gs-algo-1.2-git.jar \
 $M2/org/graphstream/gs-boids/git/gs-boids-git.jar \
-$M2/org/d3/d3-core/1.0-git/d3-core-1.0-git.jar \
 $M2/org/javassist/javassist/3.14.0-GA/javassist-3.14.0-GA.jar \
 $M2/org/graphstream/pherd/1.0/pherd-1.0.jar \
 $M2/org/graphstream/mbox2/1.0/mbox2-1.0.jar"
 MAIN="org.d3.app.boids.StartBoid"
 JVM_OPTS="-Dorg.d3.config=$CFG -Dsun.java2d.opengl=true"
-CLASSPATH="bin/"
+CLASSPATH="bin/:../d3/bin/"
 
 for LIB in $LIBS; do
 	CLASSPATH+=":$LIB"
